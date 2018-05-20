bc8bfb065a09ffa011611e48773666cc7cbb0001
PBo <sl9site@gmail.com>
2018-05-20 00:52:21 -0700

    Parser install

--- app.js
+++ app.js
@@ -5,9 +5,20 @@ const morgan = require('morgan');
 
 const productRoutes = require('./api/routes/products');
 const orderRoutes = require('./api/routes/orders');
+const bodyParser = require('body-Parser');
 
 app.use(morgan('dev'));
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
 
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Header', 'Origin, X-Request-With, Content_Type, Accept, Autorization');
+  if (req.method === 'OPTIONS') {
+    re.header('Access-Control-Allow-Method', 'PUT, POST, PATCH, DELET, GET ');
+    return res.status(200).json({});
+  }
+});
 // Routes wich should handle request
 app.use('/products', productRoutes);
 app.use('/orders', orderRoutes);
