bd962e6762b282d045e113ec613f6a191e554fd5
PBo <sl9site@gmail.com>
2018-05-26 15:04:00 -0700

    fix in progress

--- api/routes/orders.js
+++ api/routes/orders.js
@@ -1,6 +1,9 @@
 const express = require('express');
 
 const router = express.Router();
+const mongoose = require('mongoose');
+
+const Order = require('../../modules/product/productOrder');
 
 // Handle incoming GET request to /orders
 router.get('/', (req, res, next) => {
@@ -9,14 +12,24 @@ router.get('/', (req, res, next) => {
   });
 });
 router.post('/', (req, res, next) => {
-  const order = {
-    productId: req.body.productId,
+  const order = new Order({
+    _id: mongoose.Types.ObjectId(),
     quantity: req.body.quantity,
-  };
-  res.status(201).json({
-    message: 'Orders was created',
-    order,
+    product: req.body.productId,
   });
+  order
+    .save()
+    .exec()
+    .then((result) => {
+      console.log(result);
+      res.status(201).json(result);
+    })
+    .catch((err) => {
+      console.log(err);
+      res.status(500).json({
+        errror: err,
+      });
+    });
 });
 router.get('/:orderId', (req, res, next) => {
   res.status(200).json({
